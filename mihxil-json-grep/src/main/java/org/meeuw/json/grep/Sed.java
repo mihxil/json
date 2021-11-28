f441c5111ed0f050393c574ad64f849148a454b2
Michiel Meeuwissen <git@meeuw.org>
2021-11-28 16:18:15 +0100

    Implemented and tested 'sed'

--- mihxil-json-grep/src/main/java/org/meeuw/json/grep/Sed.java
+++ mihxil-json-grep/src/main/java/org/meeuw/json/grep/Sed.java
@@ -2,6 +2,7 @@ package org.meeuw.json.grep;
 
 import lombok.Getter;
 
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringJoiner;
 
@@ -10,12 +11,11 @@ import org.meeuw.json.ParseEvent;
 import org.meeuw.json.grep.matching.NeverPathMatcher;
 import org.meeuw.json.grep.matching.PathMatcher;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 
 /**
- * jsonsed. To search in json streams. You can match on the keys
- * E.g. 'rows.*.id' will result in only all 'id' values in a json structure.
- * Try the commandline option -help for an overview of all features.
+ * jsonsed. To search in json streams.
  */
 public class Sed  implements Iterator<ParseEvent> {
 
@@ -34,8 +34,6 @@ public class Sed  implements Iterator<ParseEvent> {
     }
 
 
-
-
     @Override
     public String toString() {
         return new StringJoiner(", ", Sed.class.getSimpleName() + "[", "]")
@@ -48,9 +46,14 @@ public class Sed  implements Iterator<ParseEvent> {
         return wrapped.hasNext();
     }
 
+    public void toGenerator(JsonGenerator generator) throws IOException {
+        while (hasNext()) {
+            next().toGenerator(generator);
+        }
+    }
+
     @Override
     public ParseEvent next() {
-
         ParseEvent event = wrapped.next();
         PathMatcher.MatchResult matches = matcher.matches(event);
         if (matches.getAsBoolean()) {
---
+++ mihxil-json-grep/src/test/java/org/meeuw/json/grep/SedTest.java
@@ -0,0 +1,37 @@
+package org.meeuw.json.grep;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.Test;
+
+import org.meeuw.json.Util;
+import org.meeuw.json.grep.matching.*;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SedTest {
+
+    @Test
+    public void test() throws IOException {
+        Sed sed = new Sed(
+             new PathMatcherAndChain(
+                new SinglePathMatcher(true,
+                    new PreciseMatch("items"),
+                    new PreciseMatch("a")
+                ),
+                new ValueRegexpMatcher(Pattern.compile("abc\\s*(.*)"), "$1")),
+            Util.getJsonParser("{ \"items\" : [ { \"a\" : 'abc def'},  { \"a\" : 'xyz qwv'}]}"));
+
+        StringWriter out = new StringWriter();
+        try (JsonGenerator generator = Util.getJsonFactory().createGenerator(out)) {
+            sed.toGenerator(generator);
+        }
+
+        assertThat(out.toString()).isEqualTo("{\"items\":[{\"a\":\"def\"},{\"a\":\"xyz qwv\"}]}");
+    }
+
+}
