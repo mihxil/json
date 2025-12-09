702fa6797efcd89d33c9e960b73d11d87276e167
github-action-benchmark <github@users.noreply.github.com>
2025-12-09 12:10:38 +0000

    add Benchmark (jmh) benchmark result for f5489642afd3e23d45ee844225d9036e9b61cd67

--- dev/bench/25/data.js
+++ dev/bench/25/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764864533427,
+  "lastUpdate": 1765282238341,
   "repoUrl": "https://github.com/mihxil/json",
   "entries": {
     "Benchmark": [
@@ -204,6 +204,46 @@ window.BENCHMARK_DATA = {
           {
             "name": "org.meeuw.MyBenchmark.grepMain",
             "value": 193006.78918101618,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michiel meeuwissen",
+            "name": "Michiel Meeuwissen"
+          },
+          "committer": {
+            "email": "michiel meeuwissen",
+            "name": "Michiel Meeuwissen"
+          },
+          "distinct": true,
+          "id": "f5489642afd3e23d45ee844225d9036e9b61cd67",
+          "message": "removed unused member.",
+          "timestamp": "2025-12-09T13:07:04+01:00",
+          "tree_id": "893c8738c10c334e7a7d7bfae9a4f66741c58d78",
+          "url": "https://github.com/mihxil/json/commit/f5489642afd3e23d45ee844225d9036e9b61cd67"
+        },
+        "date": 1765282237331,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.meeuw.MyBenchmark.baseline",
+            "value": 2301651279.180088,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.meeuw.MyBenchmark.grep",
+            "value": 396319.4822532449,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.meeuw.MyBenchmark.grepMain",
+            "value": 193346.51310932086,
             "unit": "ops/s",
             "extra": "iterations: 3\nforks: 1\nthreads: 1"
           }
