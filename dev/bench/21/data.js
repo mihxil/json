6a8bf1f52b1ccb06c17a80ac48abeb599d1b83a6
github-action-benchmark <github@users.noreply.github.com>
2026-09-04 17:39:01 +0000

    add Benchmark (jmh) benchmark result for 335154124195a32cfbf4f0366a267b21fe2b84ad

--- dev/bench/21/data.js
+++ dev/bench/21/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788542081528,
+  "lastUpdate": 1788543541807,
   "repoUrl": "https://github.com/mihxil/json",
   "entries": {
     "Benchmark": [
@@ -1314,6 +1314,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "org.meeuw.MyBenchmark.grepMain",
             "value": 215261.04341216487,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michiel.meeuwissen@gmail.com",
+            "name": "Michiel Meeuwissen",
+            "username": "mihxil"
+          },
+          "committer": {
+            "email": "michiel.meeuwissen@gmail.com",
+            "name": "Michiel Meeuwissen",
+            "username": "mihxil"
+          },
+          "distinct": true,
+          "id": "335154124195a32cfbf4f0366a267b21fe2b84ad",
+          "message": "A bit nicer url handling.",
+          "timestamp": "2026-09-04T19:35:34+02:00",
+          "tree_id": "1748feda6906f48b842901c2e30a926175d1b4c8",
+          "url": "https://github.com/mihxil/json/commit/335154124195a32cfbf4f0366a267b21fe2b84ad"
+        },
+        "date": 1788543541155,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.meeuw.MyBenchmark.baseline",
+            "value": 2671487377.7379375,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.meeuw.MyBenchmark.grep",
+            "value": 492069.7188450156,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.meeuw.MyBenchmark.grepMain",
+            "value": 213104.77923766748,
             "unit": "ops/s",
             "extra": "iterations: 3\nforks: 1\nthreads: 1"
           }
