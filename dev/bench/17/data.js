8ab9e8a936dd9722f9f6e652bd9df08d66063284
github-action-benchmark <github@users.noreply.github.com>
2026-09-16 11:30:58 +0000

    add Benchmark (jmh) benchmark result for 53f5953bf2e254e8b4d0cfad021c8ba3ee69c20c

--- dev/bench/17/data.js
+++ dev/bench/17/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788732363526,
+  "lastUpdate": 1789558258384,
   "repoUrl": "https://github.com/mihxil/json",
   "entries": {
     "Benchmark": [
@@ -1444,6 +1444,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "org.meeuw.MyBenchmark.grepMain",
             "value": 214602.0235479333,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "michiel.meeuwissen@gmail.com",
+            "name": "Michiel Meeuwissen",
+            "username": "mihxil"
+          },
+          "committer": {
+            "email": "michiel.meeuwissen@gmail.com",
+            "name": "Michiel Meeuwissen",
+            "username": "mihxil"
+          },
+          "distinct": true,
+          "id": "53f5953bf2e254e8b4d0cfad021c8ba3ee69c20c",
+          "message": "Updated dependency versions and corrected assembly schema references.",
+          "timestamp": "2026-09-16T13:26:07+02:00",
+          "tree_id": "09fb4bee21b67975b01b82e1b66afead9b83d923",
+          "url": "https://github.com/mihxil/json/commit/53f5953bf2e254e8b4d0cfad021c8ba3ee69c20c"
+        },
+        "date": 1789558257403,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.meeuw.MyBenchmark.baseline",
+            "value": 3886093459.7673583,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.meeuw.MyBenchmark.grep",
+            "value": 631199.7372911399,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "org.meeuw.MyBenchmark.grepMain",
+            "value": 193906.39210145638,
             "unit": "ops/s",
             "extra": "iterations: 3\nforks: 1\nthreads: 1"
           }
